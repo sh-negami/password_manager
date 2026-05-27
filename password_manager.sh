ea6240cba1011ffe7658deb2e97ecb46a48a2312
sh-negami <shumai229@gmail.com>
2026-05-27 17:30:01 +0900

    標準入力の読み取り方の修正、入力の条件付けの追加、変数の展開方法の統一

--- password_manager.sh
+++ password_manager.sh
@@ -2,25 +2,32 @@ echo "パスワードマネージャーへようこそ！"
 while true; do
     read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" action
     if [ "${action,,}" = "add password" ] || [ "${action,,}" = "a" ]; then
-        read -p "サービス名を入力してください：" s_name
-        read -p "ユーザー名を入力してください：" u_name
-        read -p "パスワードを入力してください：" p_name
+        read -r -p "サービス名を入力してください：" s_name
+        read -r -p "ユーザー名を入力してください：" u_name
+        read -r -p "パスワードを入力してください：" p_name
+        if printf "%s%s%s" "${s_name}" "${u_name}" "${p_name}" | grep -q ":"; then
+            echo "サービス名、ユーザー名、パスワードにコロンは使用できません。"
+            continue
+        elif [ -z "${s_name}" ] || [ -z "${u_name}" ] || [ -z "${p_name}" ]; then
+            echo "サービス名、ユーザー名、パスワードは空にできません。"
+            continue
+        fi
         printf "%s:%s:%s\n" "${s_name}" "${u_name}" "${p_name}" >> password.txt
 
         echo "パスワードの追加は成功しました。"
     elif [ "${action,,}" = "get password" ] || [ "${action,,}" = "g" ]; then
         is_registered=false #フラグの初期化
-        read -p "サービス名を入力してください：" target
+        read -r -p "サービス名を入力してください：" target
         while IFS=: read -r s_name u_name p_name; do
-            if [ "$s_name" = "$target" ]; then
+            if [ "${s_name}" = "${target}" ]; then
                 is_registered=true #フラグの更新
-                printf "サービス名：%s\n" "$s_name"
-                printf "ユーザー名：%s\n" "$u_name"
-                printf "パスワード：%s\n" "$p_name"
+                printf "サービス名：%s\n" "${s_name}"
+                printf "ユーザー名：%s\n" "${u_name}"
+                printf "パスワード：%s\n" "${p_name}"
                 break #同じサービス名で複数登録がある場合には最初の1件のみを表示
             fi
         done < password.txt
-        if [ "$is_registered" = false ]; then
+        if [ "${is_registered}" = "false" ]; then
             echo "そのサービスは登録されていません。"
         fi
     elif [ "${action,,}" = "exit" ] || [ "${action,,}" = "e" ]; then
