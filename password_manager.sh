7888c68d06f252e961c4853a7327d7df227f9b27
sh-negami <shumai229@gmail.com>
2026-05-27 10:48:28 +0900

    ステップ1のファイルへの書き込み機能追加

--- password_manager.sh
+++ password_manager.sh
@@ -2,4 +2,5 @@ echo "パスワードマネージャーへようこそ！"
 read -p "サービス名を入力してください：" s_name
 read -p "ユーザー名を入力してください：" u_name
 read -p "パスワードを入力してください：" p_name
+echo "${s_name}:${u_name}:${p_name}" >> password.txt
 echo "Thank you!"
