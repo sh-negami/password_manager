e49fedde6b2f9087147b21315d51159f76d381ad
sh-negami <shumai229@gmail.com>
2026-05-27 16:04:58 +0900

    Get Passwordの処理を終えた後、初めに戻るように修正

--- password_manager.sh
+++ password_manager.sh
@@ -9,16 +9,20 @@ while true; do
 
         echo "パスワードの追加は成功しました。"
     elif [ "${action,,}" = "get password" ] || [ "${action,,}" = "g" ]; then
+        is_registered=false #フラグの初期化
         read -p "サービス名を入力してください：" target
         while IFS=: read -r s_name u_name p_name; do
             if [ "$s_name" = "$target" ]; then
+                is_registered=true #フラグの更新
                 printf "サービス名：%s\n" "$s_name"
                 printf "ユーザー名：%s\n" "$u_name"
                 printf "パスワード：%s\n" "$p_name"
-                break 2
+                break #同じサービス名で複数登録がある場合には最初の1件のみを表示
             fi
         done < password.txt
-        echo "そのサービスは登録されていません。"
+        if [ "$is_registered" = false ]; then
+            echo "そのサービスは登録されていません。"
+        fi
     elif [ "${action,,}" = "exit" ] || [ "${action,,}" = "e" ]; then
         echo "Thank you!"
         break
